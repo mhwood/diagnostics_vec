85dab2c8ff0bb20292bbea15425fa4e0e41be5a6
Mike Wood <mhwood@uci.edu>
2021-11-17 09:59:24 -0800

    adding max iter cap option and a mask check

---
+++ verification/tutorial_global_oce_latlon/input_dv/create_masks.py
@@ -0,0 +1,42 @@
+
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+domain_shape = (40,90)
+
+# create the eastern mask
+mask = np.zeros(domain_shape)
+counter = 1
+col = 45
+for row in range(domain_shape[0]):
+    mask[row,col] = counter
+    counter+=1
+file_name = os.path.join(os.getcwd(),'prime_meridian_mask.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
+
+
+# create the equator mask
+mask = np.zeros(domain_shape)
+counter = 1
+row = 20
+for col in range(domain_shape[1]):
+    mask[row,col] = counter
+    counter+=1
+file_name = os.path.join(os.getcwd(),'equator_mask.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
+
+# create the surface mask
+min_row = 10
+max_row = 25
+min_col= 10
+max_col = 30
+mask = np.zeros(domain_shape)
+counter = 1
+for row in range(min_row, max_row + 1):
+    for col in range(min_col,max_col+1):
+        mask[row,col] = counter
+        counter+=1
+file_name = os.path.join(os.getcwd(),'indian_ocean_mask.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
+
