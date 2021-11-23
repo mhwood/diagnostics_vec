88953cde479427b8454e1ea76c117786f45efd85
Mike Wood <mhwood@uci.edu>
2021-11-23 10:41:47 -0800

    updating verification for variable periods

--- verification/global_ocean.cs32x15/input_dv.seaice/create_masks.py
+++ verification/global_ocean.cs32x15/input_dv.seaice/create_masks.py
@@ -1,78 +1,72 @@
+
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-def read_cs32_faces(input_dir):
-    XCs = []
-    YCs = []
+domain_shape = (40,90)
 
-    for i in range(1, 7):
-        test_file = os.path.join(input_dir, 'grid_cs32.face' + '{:03d}'.format(i) + '.bin')
+# create the eastern mask
+mask = np.zeros(domain_shape)
+counter = 1
+col = 45
+for row in range(domain_shape[0]):
+    mask[row,col] = counter
+    counter+=1
 
-        grid = np.fromfile(test_file, dtype='>f8')
-        grid = np.reshape(grid, (18, 33, 33))
+file_name = os.path.join(os.getcwd(),'mask1_ctrl.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
 
-        XC = grid[0][:-1, :-1]
-        YC = grid[1][:-1, :-1]
-        XCs.append(XC)
-        YCs.append(YC)
+file_name = os.path.join(os.getcwd(),'mask1_test1.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
 
-        XC = np.reshape(XC, (32, 1, 32))
-        YC = np.reshape(YC, (32, 1, 32))
-        if i == 1:
-            X = XC
-            Y = YC
-        else:
-            X = np.concatenate([X, XC], axis=1)
-            Y = np.concatenate([Y, YC], axis=1)
+file_name = os.path.join(os.getcwd(),'mask1_test2.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
 
-    X[X<0]+=360
+file_name = os.path.join(os.getcwd(),'mask1_test3.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
 
-    return(X,Y)
-
-def create_mask_from_latlon(X,Y,lon,lat):
-    X = np.reshape(X, (np.size(X), 1))
-    Y = np.reshape(Y, (np.size(Y), 1))
-    mask = np.zeros_like(X)
-    last_index = -100000000
-    counter = 0
-    for ll in range(len(lon)):
-        dist = ((X - lon[ll]) ** 2 + (Y - lat[ll]) ** 2) ** 0.5
-        index = np.argmin(dist)
-        if index != last_index:
-            counter += 1
-            last_index = index
-            mask[index] = counter
-    return(mask)
-
-def create_south_pole_mask(X,Y):
-
-    mask = np.zeros_like(X)
-    counter = 0
-    for i in range(1,31):
-        for j in range(1,31):
-            counter +=1
-            mask[i,5,j] = counter
-    mask = np.reshape(mask, (np.size(mask), 1))
-    return(mask)
-
-
-input_dir = os.getcwd()
-
-X,Y = read_cs32_faces(input_dir)
 
 # create the equator mask
-lon = np.arange(360)
-lat = np.zeros_like(lon)
-mask = create_mask_from_latlon(X,Y,lon,lat)
-output_file = input_dir + '/equator_mask.bin'
-mask.astype('>f8').tofile(output_file)
-print('    Equator mask contains '+str(np.sum(mask>0))+' points')
+mask = np.zeros(domain_shape)
+counter = 1
+row = 20
+for col in range(domain_shape[1]):
+    mask[row,col] = counter
+    counter+=1
 
-X,Y = read_cs32_faces(input_dir)
+file_name = os.path.join(os.getcwd(),'mask2_ctrl.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
 
-# create the south pole mask
-mask = create_south_pole_mask(X,Y)
-output_file = input_dir + '/south_pole_mask.bin'
-mask.astype('>f8').tofile(output_file)
-print('    South pole mask contains '+str(np.sum(mask>0))+' points')
+file_name = os.path.join(os.getcwd(),'mask2_test1.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
+
+file_name = os.path.join(os.getcwd(),'mask2_test2.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
+
+file_name = os.path.join(os.getcwd(),'mask2_test3.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
+
+# create the surface mask
+min_row = 10
+max_row = 25
+min_col= 10
+max_col = 30
+mask = np.zeros(domain_shape)
+counter = 1
+for row in range(min_row, max_row + 1):
+    for col in range(min_col,max_col+1):
+        mask[row,col] = counter
+        counter+=1
+
+file_name = os.path.join(os.getcwd(),'mask3_ctrl.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
+
+file_name = os.path.join(os.getcwd(),'mask3_test1.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
+
+file_name = os.path.join(os.getcwd(),'mask3_test2.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
+
+file_name = os.path.join(os.getcwd(),'mask3_test3.bin')
+mask.ravel(order='C').astype('>f4').tofile(file_name)
+
